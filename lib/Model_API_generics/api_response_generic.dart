6574694aa3fea9475ff5e9edfa542267a93560df
Cleidson <cleidson.mac@outlook.com>
2024-12-24 18:05:05 +0000

    First Try Consuming Rest API - Error - Don't Load anything!

--- lib/Model_API_generics/api_response_generic.dart
+++ lib/Model_API_generics/api_response_generic.dart
@@ -1,13 +1,13 @@
 class ApiResponseGeneric<T> {
 
-  final T data;
-  final bool error;
-  final String errorMessage;
+  final T? data;
+  final bool? error;
+  final String? errorMessage;
 
   ApiResponseGeneric({
-    required this.data,
-    required this.error,
-    required this.errorMessage,
+    this.data,
+    this.error,
+    this.errorMessage,
   });
 
 }
--- lib/pages/a_rest_api_basic_ consuming/note_list_page.dart
+++ lib/pages/a_rest_api_basic_ consuming/note_list_page.dart
@@ -1,13 +1,14 @@
-// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: non_constant_identifier_names, unused_element
 
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:my_imc_calc_app/Model/note_entity.dart';
+import 'package:my_imc_calc_app/Model_API_generics/api_response_generic.dart';
 import 'package:my_imc_calc_app/pages/constants/constants_library.dart';
 import 'package:my_imc_calc_app/service/notes_service.dart';
 
-
- final notes_old = [ //..................................#### THIS ONE IS THE FIRST APPROACH TO GET DATA FROM A ARRAY LIST OF DATA, JUT TO UNDERSTAND THE CONCEPT ####
+final notes_old = [
+  //..................................#### THIS ONE IS THE FIRST APPROACH TO GET DATA FROM A ARRAY LIST OF DATA, JUT TO UNDERSTAND THE CONCEPT ####
 
   NoteEntity(
     noteID: "1",
@@ -36,18 +37,24 @@ import 'package:my_imc_calc_app/service/notes_service.dart';
     createDateTime: DateTime.now(),
     latestEditDateTime: DateTime.now(),
   )
+];
 
- ];
- 
- final notes_old2 = NotesService().getNotesList(); //....#### THIS ONE IS A BASIC EXAMPLE TO GET DATA FROM A SERVICE CLASS ####
+final notes_old2 = NotesService()
+    .getNotesList(); //....#### THIS ONE IS A BASIC EXAMPLE TO GET DATA FROM A SERVICE CLASS ####
 
- NotesService get service => GetIt.I<NotesService>(); // #### HERE USING THE SETUP LOCATOR IN THIS PAGE! ####
- List<NoteEntity> notes = []; //........................ #### HERE STARTING THE LIST OF NOTES EMPTY ####
+NotesService get service => GetIt.I<
+    NotesService>(); // #### HERE USING THE SETUP LOCATOR IN THIS PAGE! ####
+
+ApiResponseGeneric<List<NoteEntity>> _apiResponseGeneric =
+    [] as ApiResponseGeneric<List<NoteEntity>>;
+bool _isLoading = false;
+
+//List<NoteEntity> notes = []; //........................ #### HERE STARTING THE LIST OF NOTES EMPTY ####
 
 // DATE FORMATER...
- String formatDateTime(DateTime dateTime) {
+String formatDateTime(DateTime dateTime) {
   return '${dateTime.day}/${dateTime.month}/${dateTime.year}';
- }
+}
 
 class NoteListPage extends StatefulWidget {
   const NoteListPage({super.key, required this.title});
@@ -58,11 +65,23 @@ class NoteListPage extends StatefulWidget {
 }
 
 class _NoteListPageState extends State<NoteListPage> {
-
   @override
   void initState() {
-    notes =service.getNotesList();
+    // notes =service.getNotesList();
+    _fetchNotes();
     super.initState();
+  }
+
+  _fetchNotes() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    _apiResponseGeneric = await service.getNotesList();
+
+    setState(() {
+      _isLoading = false;
+    });
   }
 
   @override
@@ -77,19 +96,32 @@ class _NoteListPageState extends State<NoteListPage> {
         onPressed: () {},
         child: const Icon(Icons.add),
       ),
-      body: ListView.separated(
-        separatorBuilder: (_, __) => const Divider(
-          height: 1,
-          color: Colors.black,
-        ),
-        itemBuilder: (_, index) {
-          return ListTile(
-            title: Text(notes[index].noteTitle, style: kTxtTitleListTextStyle),
-            subtitle: Text('Last edited on ${formatDateTime(notes[index].latestEditDateTime)}'),
-          );
-        },
-        itemCount: notes.length,
-      ),
+      body: Builder(builder: (_) {
+
+        if(_isLoading){
+          return const CircularProgressIndicator();
+        }
+
+        if(_apiResponseGeneric.error!) {
+          return const Center(child: Text('An Error: KKKKKK'));
+        }
+
+        return ListView.separated(
+          separatorBuilder: (_, __) => const Divider(
+            height: 1,
+            color: Colors.black,
+          ),
+          itemBuilder: (_, index) {
+            return ListTile(
+              title: Text(_apiResponseGeneric.data![index].noteTitle,
+                  style: kTxtTitleListTextStyle),
+              subtitle: Text(
+                  'Last edited on ${formatDateTime(_apiResponseGeneric.data![index].latestEditDateTime)}'),
+            );
+          },
+          itemCount: _apiResponseGeneric.data!.length,
+        );
+      }),
     );
   }
 }
