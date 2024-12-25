c34776c98fc36196d4e6bc14dbf8f47c82ae4350
Cleidson <cleidson.mac@outlook.com>
2024-12-25 12:14:41 +0000

    I just added study notes to better understand what the solutions I made are

--- lib/service/notes_service_bkp_copy_error_loading.dart
+++ lib/service/notes_service_bkp_copy_error_loading.dart
@@ -3,31 +3,35 @@ import 'dart:convert';
 import 'package:my_imc_calc_app/Model/note_entity.dart';
 import 'package:my_imc_calc_app/Model_API_generics/api_response_generic.dart';
 import 'package:http/http.dart' as http;
-// import 'package:my_imc_calc_app/pages/a_rest_api_basic_%20consuming/note_list_page.dart';
+
+//THE ERRO HERE was BECAUSE THIS..
+// return await http.get((API + '/notes') as Uri, headers: null)
+// I NEED TO USING PARSE, like this bellow
+// final response = await http.get(Uri.parse(API + '/notes'));
 
 class NotesServiceErrorNoLoadingNotyhing {
   static const API = 'https://6767d711c1de2e6421c86392.mockapi.io/api/v1';
-  
+
   get notes => [];
 
   Future<ApiResponseGeneric<List<NoteEntity>>> getNotesList() async {
-
-    return http.get((API + '/notes') as Uri, headers: null)
-    .then((data) {
-      if(data.statusCode == 200){
-        
+    return await http
+        .get(Uri.parse(API + '/notes'), headers: null)
+        .then((data) {
+      if (data.statusCode == 200) {
         final jsonData = json.decode(data.body);
         final notes = <NoteEntity>[];
 
-          for (var item in jsonData){
-            final note = NoteEntity(
-              noteID: item['noteID'],
-              noteTitle: item['noteTitle'], 
-              createDateTime: DateTime.parse(item['createDateTime']),
-              latestEditDateTime: DateTime.parse(item['latestEditDateTime']), //THE ERROR IS HERE!!???
-            );
-            notes.add(note);
-          }
+        for (var item in jsonData) {
+          final note = NoteEntity(
+            noteID: item['noteID'],
+            noteTitle: item['noteTitle'],
+            createDateTime: DateTime.parse(item['createDateTime']),
+            latestEditDateTime: DateTime.parse(
+                item['latestEditDateTime']), //THE ERROR IS HERE!!???
+          );
+          notes.add(note);
+        }
 
         return ApiResponseGeneric<List<NoteEntity>>(
           data: notes,
@@ -36,13 +40,15 @@ class NotesServiceErrorNoLoadingNotyhing {
         );
       }
       return ApiResponseGeneric<List<NoteEntity>>(
-          data: [],
-          error: true,
-          errorMessage:
-              'Failed to fetch notes. Status Code: ${data.statusCode}',
-        );
-    })
-    .catchError((_) => ApiResponseGeneric<List<NoteEntity>>(data: notes, error: true, errorMessage: 'An Error Occured 2')); 
+        data: [],
+        error: true,
+        errorMessage: 'Failed to fetch notes. Status Code: ${data.statusCode}',
+      );
+    }).catchError(
+      (_) => ApiResponseGeneric<List<NoteEntity>>(
+          data: notes, 
+          error: true, 
+          errorMessage: 'An Error Occured!'),
+    );
   }
-
 }
