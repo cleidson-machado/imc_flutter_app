4497b1ac17005cf0b4e209ff2d4ca53436513dbb
Cleidson <cleidson.mac@outlook.com>
2024-12-26 16:49:41 +0000

    HERE I AM add a SingleChildScrollView for Study

--- lib/pages/my_index_projects_page.dart
+++ lib/pages/my_index_projects_page.dart
@@ -1,10 +1,9 @@
-// ignore_for_file: avoid_unnecessary_containers
-
 import 'package:flutter/material.dart';
 import 'package:my_imc_calc_app/pages/combo_from_api_a/combo_index_page_one.dart';
 import 'package:my_imc_calc_app/pages/components/button_index_container_component.dart';
 import 'package:my_imc_calc_app/pages/constants/constants_library.dart';
 
+//HERE I AM add a SingleChildScrollView for Study..
 
 class MyIndexProjectsPage extends StatelessWidget {
   const MyIndexProjectsPage({super.key});
@@ -17,45 +16,154 @@ class MyIndexProjectsPage extends StatelessWidget {
         elevation: 2.5,
         title: const Text('Index Project Choices'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 5.0),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.of(context).pushNamed('/calc');
-              },
-              child: const ButtonIndexContainerComponent(txtButtonTitle: 'imc calculator'),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        padding: const EdgeInsets.all(5.0),
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Padding(
+                padding: EdgeInsets.only(top: 5.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Type the Keyword or for a Search...',
+                    hintStyle:
+                        TextStyle(fontWeight: FontWeight.w500, fontSize: 18.0),
+                  ),
+                )),
+            Padding(
+              padding: const EdgeInsets.only(top: 5.0),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.of(context).pushNamed('/calc');
+                },
+                child: const ButtonIndexContainerComponent(
+                    txtButtonTitle: 'imc calculator'),
+              ),
             ),
-          ),
-          GestureDetector(
-            onTap: () {
-               Navigator.push(
-                 context,
-                 MaterialPageRoute(
-                   builder: (context) =>
-                       const ComboIndexPageOne(title: theProjectTitleOne),
-                 ),
-               );
-             },
-
-            child: const ButtonIndexContainerComponent(txtButtonTitle: 'combos from Api BR'),
-          ),
-          GestureDetector(
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        const ComboIndexPageOne(title: theProjectTitleOne),
+                  ),
+                );
+              },
+              child: const ButtonIndexContainerComponent(
+                  txtButtonTitle: 'combos from Api BR'),
+            ),
+            GestureDetector(
               onTap: () {
                 Navigator.of(context).pushNamed('/notesHomePage');
               },
-              child: const ButtonIndexContainerComponent(txtButtonTitle: 'data from api Exp-01'),
+              child: const ButtonIndexContainerComponent(
+                  txtButtonTitle: 'data from api Exp-01'),
             ),
             GestureDetector(
               onTap: () {
                 Navigator.of(context).pushNamed('/calc');
               },
-              child: const ButtonIndexContainerComponent(txtButtonTitle: 'todo two...'),
+              child: const ButtonIndexContainerComponent(
+                  txtButtonTitle: 'todo 01...'),
             ),
-        ],
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushNamed('/calc');
+              },
+              child: const ButtonIndexContainerComponent(
+                  txtButtonTitle: 'todo 02...'),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushNamed('/calc');
+              },
+              child: const ButtonIndexContainerComponent(
+                  txtButtonTitle: 'todo 03...'),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushNamed('/calc');
+              },
+              child: const ButtonIndexContainerComponent(
+                  txtButtonTitle: 'todo 04...'),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushNamed('/calc');
+              },
+              child: const ButtonIndexContainerComponent(
+                  txtButtonTitle: 'todo 05...'),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushNamed('/calc');
+              },
+              child: const ButtonIndexContainerComponent(
+                  txtButtonTitle: 'todo 06...'),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushNamed('/calc');
+              },
+              child: const ButtonIndexContainerComponent(
+                  txtButtonTitle: 'todo 07...'),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushNamed('/calc');
+              },
+              child: const ButtonIndexContainerComponent(
+                  txtButtonTitle: 'todo 08...'),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushNamed('/calc');
+              },
+              child: const ButtonIndexContainerComponent(
+                  txtButtonTitle: 'todo 09...'),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushNamed('/calc');
+              },
+              child: const ButtonIndexContainerComponent(
+                  txtButtonTitle: 'todo 10...'),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushNamed('/calc');
+              },
+              child: const ButtonIndexContainerComponent(
+                  txtButtonTitle: 'todo 11...'),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushNamed('/calc');
+              },
+              child: const ButtonIndexContainerComponent(
+                  txtButtonTitle: 'todo 12...'),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushNamed('/calc');
+              },
+              child: const ButtonIndexContainerComponent(
+                  txtButtonTitle: 'todo 14...'),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushNamed('/calc');
+              },
+              child: const ButtonIndexContainerComponent(
+                  txtButtonTitle: 'todo 15...'),
+            ),
+          ],
+        ),
       ),
     );
   }
