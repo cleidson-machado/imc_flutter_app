d8709a3138102b431c763da8c245072410755c4f
Cleidson <cleidson.mac@outlook.com>
2025-02-25 21:46:38 +0000

    Add al the stuff for translations in this Basic Sketch Project

---
+++ lib/resources/translation/app_localizations_pt.dart
@@ -0,0 +1,14 @@
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Portuguese (`pt`).
+class AppLocalizationsPt extends AppLocalizations {
+  AppLocalizationsPt([String locale = 'pt']) : super(locale);
+
+  @override
+  String get hello => 'Olá';
+
+  @override
+  String get welcome => 'Bem-vindo';
+}
