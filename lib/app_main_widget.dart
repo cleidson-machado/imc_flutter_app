43c38ded805cfcc3a809751c8fc934cde070254a
Cleidson <cleidson.mac@outlook.com>
2025-02-28 07:44:34 +0000

    Add a Basic Structure divided by scope

---
+++ lib/app_main_widget.dart
@@ -0,0 +1,33 @@
+
+// ignore_for_file: avoid_print
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:my_imc_calc_app/screens/home_screen.dart';
+import 'package:my_imc_calc_app/screens/login_screen.dart';
+import 'package:my_imc_calc_app/theme/app_theme.dart';
+import 'package:provider/provider.dart';
+
+class AppMainWidget extends StatefulWidget {
+  const AppMainWidget({super.key});
+
+  @override
+  State<AppMainWidget> createState() => _AppMainWidgetState();
+}
+
+class _AppMainWidgetState extends State<AppMainWidget> {
+  @override
+  Widget build(BuildContext context) {
+    print(dotenv.env['API_END_POIN_TEST']); //############# TEST API USE!!..
+    print(dotenv.env); //################################## Print all environment variables..
+    return Consumer<AppTheme>(
+      builder: (context, appTheme, child) {
+        return CupertinoApp(
+          debugShowCheckedModeBanner: false, //############ THE ORIGINAL DEBUG FLAG...
+          theme: appTheme.themeData,
+          home: const HomeScreen(),
+        );
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,8 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:my_imc_calc_app/screens/settings_screen.dart';
+import 'package:my_imc_calc_app/app_main_widget.dart';
 import 'package:my_imc_calc_app/theme/app_theme.dart';
-import 'package:my_imc_calc_app/screens/home_screen.dart';
-import 'package:my_imc_calc_app/screens/login_screen.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
@@ -14,26 +12,7 @@ void main() async {
   runApp(
     ChangeNotifierProvider(
       create: (context) => AppTheme(),
-      child: const MyApp(),
+      child: const AppMainWidget(),
     ),
   );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    print(dotenv.env['API_END_POIN_TEST']); //############# TEST API USE!!..
-    print(dotenv.env); //################################## Print all environment variables..
-    return Consumer<AppTheme>(
-      builder: (context, appTheme, child) {
-        return CupertinoApp(
-          debugShowCheckedModeBanner: false, //############# THE ORIGINAL DEBUG FLAG...
-          theme: appTheme.themeData,
-          home: const LoginScreen(),
-        );
-      },
-    );
-  }
 }
