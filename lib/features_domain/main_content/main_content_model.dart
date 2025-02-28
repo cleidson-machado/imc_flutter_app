97c152091e48ba086499df50ec48d48e3d15a5f9
Cleidson <cleidson.mac@outlook.com>
2025-02-28 11:19:04 +0000

    Add Just Basic Model Class for Dev Tests Phase 001

---
+++ lib/features_domain/main_content/main_content_model.dart
@@ -0,0 +1,93 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class MainContentModel {
+  
+  final String id;
+  final String title;
+  final String subTitle;
+  final String description;
+  final String contentImageUrl;
+  final String bioAnnotationInfo;
+
+  MainContentModel({
+    required this.id,
+    required this.title,
+    required this.subTitle,
+    required this.description,
+    required this.contentImageUrl,
+    required this.bioAnnotationInfo,
+  });
+
+  MainContentModel copyWith({
+    String? id,
+    String? title,
+    String? subTitle,
+    String? description,
+    String? contentImageUrl,
+    String? bioAnnotationInfo,
+  }) {
+    return MainContentModel(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      subTitle: subTitle ?? this.subTitle,
+      description: description ?? this.description,
+      contentImageUrl: contentImageUrl ?? this.contentImageUrl,
+      bioAnnotationInfo: bioAnnotationInfo ?? this.bioAnnotationInfo,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'title': title,
+      'subTitle': subTitle,
+      'description': description,
+      'contentImageUrl': contentImageUrl,
+      'bioAnnotationInfo': bioAnnotationInfo,
+    };
+  }
+
+  factory MainContentModel.fromMap(Map<String, dynamic> map) {
+    return MainContentModel(
+      id: map['id'] as String,
+      title: map['title'] as String,
+      subTitle: map['subTitle'] as String,
+      description: map['description'] as String,
+      contentImageUrl: map['contentImageUrl'] as String,
+      bioAnnotationInfo: map['bioAnnotationInfo'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory MainContentModel.fromJson(String source) => MainContentModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'MainContentModel(id: $id, title: $title, subTitle: $subTitle, description: $description, contentImageUrl: $contentImageUrl, bioAnnotationInfo: $bioAnnotationInfo)';
+  }
+
+  @override
+  bool operator ==(covariant MainContentModel other) {
+    if (identical(this, other)) return true;
+  
+    return 
+      other.id == id &&
+      other.title == title &&
+      other.subTitle == subTitle &&
+      other.description == description &&
+      other.contentImageUrl == contentImageUrl &&
+      other.bioAnnotationInfo == bioAnnotationInfo;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+      title.hashCode ^
+      subTitle.hashCode ^
+      description.hashCode ^
+      contentImageUrl.hashCode ^
+      bioAnnotationInfo.hashCode;
+  }
+}
