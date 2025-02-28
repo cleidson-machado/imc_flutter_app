efcdff759aca996185f8139551662cca5dad53d3
Cleidson <cleidson.mac@outlook.com>
2025-02-28 06:59:00 +0000

    OK FIX TEH .ENV LOADING ERROR.. THE PUBSPEC.YMAL WHAS incorrectly indented....

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:my_imc_calc_app/screens/settings_screen.dart';
 import 'package:my_imc_calc_app/theme/app_theme.dart';
 import 'package:my_imc_calc_app/screens/home_screen.dart';
 import 'package:my_imc_calc_app/screens/login_screen.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); //############# Function to ensure Flutter is initialized before loading the .ENV file!!..
+  await dotenv.load(fileName: ".env");
   runApp(
     ChangeNotifierProvider(
       create: (context) => AppTheme(),
@@ -19,10 +24,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print(dotenv.env['API_END_POIN_TEST']); //############# TEST API USE!!..
+    print(dotenv.env); //################################## Print all environment variables..
     return Consumer<AppTheme>(
       builder: (context, appTheme, child) {
         return CupertinoApp(
-          debugShowCheckedModeBanner: false,
+          debugShowCheckedModeBanner: false, //############# THE ORIGINAL DEBUG FLAG...
           theme: appTheme.themeData,
           home: const LoginScreen(),
         );
